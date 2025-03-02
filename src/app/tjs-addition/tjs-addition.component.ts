44aaa159470317e0a32e258a9d390a9dc01b1b97
tsteelematc <tsteele@madisoncollege.edu>
2025-03-01 21:14:53 -0600

    Generated my addition component...

---
+++ src/app/tjs-addition/tjs-addition.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tjs-addition',
+  standalone: false,
+  templateUrl: './tjs-addition.component.html',
+  styleUrl: './tjs-addition.component.css'
+})
+export class TjsAdditionComponent {
+
+}
