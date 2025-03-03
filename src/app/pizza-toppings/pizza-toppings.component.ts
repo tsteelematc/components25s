74edb4cc167496d428cdc8ee0d3f78812ea0b3f4
Justin Gritton-Bell <jgrittonbell@madisoncollege.edu>
2025-03-02 19:04:45 -0600

    Prepping for next vid...
    ng generate component no longer ads these items....

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pizza-toppings',
   standalone: false,
   templateUrl: './pizza-toppings.component.html',
-  styleUrl: './pizza-toppings.component.css'
+  styleUrl: './pizza-toppings.component.css',
 })
-export class PizzaToppingsComponent {
-
+export class PizzaToppingsComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
 }
