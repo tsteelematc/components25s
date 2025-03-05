98ae84dad0c46197c08c73fc235bc9a7abb7fce9
MNuenninghoff <mikenue@gmail.com>
2025-03-04 21:24:27 -0600

    imported mat modules needed for form fields

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import {MatInputModule} from '@angular/material/input';
+import { MatFormField } from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
 import { MnFivekPaceComponent } from './mn-fivek-pace/mn-fivek-pace.component';
 
 @NgModule({
@@ -25,6 +28,9 @@ import { MnFivekPaceComponent } from './mn-fivek-pace/mn-fivek-pace.component';
     , MatCardModule
     , MatButtonModule
     , MatCheckboxModule
+    , MatInputModule
+    , MatIconModule
+    , MatFormField
   ],
   providers: [],
   bootstrap: [AppComponent]
