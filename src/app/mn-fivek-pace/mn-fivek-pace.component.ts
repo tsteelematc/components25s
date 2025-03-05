ef6838a2059e93ca26c87f618f17c7a7eabf56fc
MNuenninghoff <mikenue@gmail.com>
2025-03-04 21:39:13 -0600

    added form fields to input time

--- src/app/mn-fivek-pace/mn-fivek-pace.component.ts
+++ src/app/mn-fivek-pace/mn-fivek-pace.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-mn-fivek-pace',
@@ -6,6 +6,20 @@ import { Component } from '@angular/core';
   templateUrl: './mn-fivek-pace.component.html',
   styleUrl: './mn-fivek-pace.component.css'
 })
-export class MnFivekPaceComponent {
+export class MnFivekPaceComponent implements OnInit {
 
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
+  milesIn5k = 3.125;
+
+  minutes = 0;
+  seconds = 0;
+  pace = 0;
+
+  calculatePace = () => {
+    this.pace = (this.minutes + (this.seconds / 60)) / this.milesIn5k;
+  }
 }
