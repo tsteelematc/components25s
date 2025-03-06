4442be87199f120fdc396af631a1376a09e0d02b
Carly Anderson <c4rlyanderson@gmail.com>
2025-03-06 14:40:47 -0600

    Calling a DI'd service
    We have displayed topping in console, next we want to display them to component(page?).

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PizzaService } from '../pizza.service';
 
 @Component({
   selector: 'app-pizza-toppings',
@@ -8,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PizzaToppingsComponent implements OnInit {
 
-constructor () {}
+  //Magic DI or dependence injection
+  //Getting access to pizza service
+  constructor (
+    private pizzaSvc: PizzaService
+  ) { }
 
+  //angular method, called when ready to initialize
   ngOnInit(): void {
+    const pt = this.pizzaSvc.getPizzaToppingsFromTheCloud();
+    console.log(pt);
       
   }
 }
--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -6,4 +6,23 @@ import { Injectable } from '@angular/core';
 export class PizzaService {
 
   constructor() { }
+
+  getPizzaToppingsFromTheCloud() {
+    //ATM just pretending to do this until later
+    const toppingsFromWebService = [
+      {
+        name: 'Olives'
+        , price: 2.25
+      }
+      , {
+        name: 'Pepperoni'
+        , price: 1.75
+      }
+      , {
+        name: 'Banana Peppers'
+        , price: 1.50
+      }
+    ];
+    return toppingsFromWebService;
+  }
 }
