69eb81669b750fffb405686a0fe37c64727ac6a8
Emily Gimm <egimm@madisoncollege.edu>
2025-03-06 17:18:41 -0600

    Generated date component

---
+++ src/app/date/date.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-date',
+  standalone: false,
+  templateUrl: './date.component.html',
+  styleUrl: './date.component.css'
+})
+export class DateComponent {
+
+}
