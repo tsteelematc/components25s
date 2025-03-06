8a825d05482d668c9ed15b036c2c6bf1f5ec7605
Emily Gimm <egimm@madisoncollege.edu>
2025-03-06 17:35:55 -0600

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,25 @@ import { AppComponent } from './app.component';
 import { PizzaToppingsComponent } from './pizza-toppings/pizza-toppings.component';
 
 import { FormsModule } from '@angular/forms';
-import { DateComponent } from './date/date.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaToppingsComponent,
-    DateComponent
+    PizzaToppingsComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
+    , AppRoutingModule
     , FormsModule
+    , BrowserAnimationsModule
+    , MatCardModule
+    , MatButtonModule
+    , MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
