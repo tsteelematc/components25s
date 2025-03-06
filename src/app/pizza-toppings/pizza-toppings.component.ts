54b62439c20a3b311a6a71ccd47c7874346bb27c
Carly Anderson <c4rlyanderson@gmail.com>
2025-03-06 14:07:31 -0600

    Generated pizza topping component
    command propt: ng generate component pizza-toppings --standalone=false

---
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pizza-toppings',
+  standalone: false,
+  templateUrl: './pizza-toppings.component.html',
+  styleUrl: './pizza-toppings.component.css'
+})
+export class PizzaToppingsComponent {
+
+}
