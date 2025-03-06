72e46148283ae31d44a1c6310b19c14aa504260d
Emily Gimm <egimm@madisoncollege.edu>
2025-03-06 17:32:03 -0600

    Update pizza-toppings.component.ts

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -48,4 +48,19 @@ export class PizzaToppingsComponent implements OnInit {
       )
     ;
   }
+
+  checkAll = () => this.availablePizzaToppings = this.availablePizzaToppings.map(
+    x => ({
+      ...x
+      ,checked: true
+    })
+  );
+
+  uncheckAll = () => this.availablePizzaToppings = this.availablePizzaToppings.map(
+    x => ({
+      ...x
+      ,checked: false
+    })
+  );
+
 }
