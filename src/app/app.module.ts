ee9f201ad66b8edaf296ab95cd686344e8871966
Justin Gritton-Bell <jgrittonbell@madisoncollege.edu>
2025-03-11 17:13:06 -0500

    Added loading spinner...
    The loading spinner works!! but the button should disappear when clicked and then reappear after the answer shows up.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,14 @@ import { TjsAdditionComponent } from './tjs-addition/tjs-addition.component'; //
 
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CommonModule } from '@angular/common'; 
+import { CommonModule } from '@angular/common';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { JgbMagic8BallComponent } from './jgb-magic-8-ball/jgb-magic-8-ball.component';
 
 @NgModule({
@@ -37,8 +38,9 @@ import { JgbMagic8BallComponent } from './jgb-magic-8-ball/jgb-magic-8-ball.comp
     MatCheckboxModule,
     MatInputModule,
     MatFormFieldModule,
+    MatProgressSpinnerModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/jgb-magic-8-ball/jgb-magic-8-ball.component.ts
+++ src/app/jgb-magic-8-ball/jgb-magic-8-ball.component.ts
@@ -22,18 +22,33 @@ export class JgbMagic8BallComponent {
   //Creates a answer property that will have the type of a string(our answer) or null(no answer has been picked yet) This also has the default of null so that no answer shows up in the view to start with
   answer: string | null = null;
 
+  //Creates a loading property for the loading spinner UI
+  loading: boolean = false;
+
   //This function sets the answer property to a random value from the answers array.
   shakeBallOld = () =>
     (this.answer =
       this.answers[Math.floor(Math.random() * this.answers.length)]);
 
   //This function does the same thing as the shakeBallOld but adds a delay
-  shakeBall = () => {
+  shakeBallOld2 = () => {
     this.answer = null;
 
     setTimeout(() => {
       this.answer =
         this.answers[Math.floor(Math.random() * this.answers.length)];
+    }, 1500);
+  };
+
+  //This function does the same thing as the shakeBallOld2 but adds logic for a loading spinner
+  shakeBall = () => {
+    this.answer = null;
+    this.loading = true;
+
+    setTimeout(() => {
+      this.answer =
+        this.answers[Math.floor(Math.random() * this.answers.length)];
+      this.loading = false;
     }, 1500);
   };
 }
