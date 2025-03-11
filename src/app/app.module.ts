c558af0dd2ac12fe3f9412bfa2cfdcb9b772f048
Justin Gritton-Bell <jgrittonbell@madisoncollege.edu>
2025-03-11 14:35:40 -0500

    Updated app.module.ts
    The browser animation along with many other imports did get automaticly added with the material installation. I think this was because it "Skipping installation: Package already installed"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+// import { BrowserAnimationsModule } from '@angular/plateform-browser/animations'; Still might need to add this to imports but this caused an error.
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PizzaToppingsComponent } from './pizza-toppings/pizza-toppings.component';
 
 import { FormsModule } from '@angular/forms';
 @NgModule({
-  declarations: [
-    AppComponent,
-    PizzaToppingsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-    , FormsModule
-  ],
+  declarations: [AppComponent, PizzaToppingsComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
