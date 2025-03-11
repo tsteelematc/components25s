c4a282fa98fd12009c329bce930bc52be4ddf4dc
Justin Gritton-Bell <jgrittonbell@madisoncollege.edu>
2025-03-11 15:45:55 -0500

    Check all and uncheck all feature implemented...
    Still having issues with color themes

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -36,4 +36,16 @@ export class PizzaToppingsComponent implements OnInit {
       .filter((x) => x.checked)
       .reduce((acc, x) => acc + x.price, 0);
   }
+
+  checkAll = () =>
+    (this.availablePizzaToppings = this.availablePizzaToppings.map((x) => ({
+      ...x,
+      checked: true,
+    })));
+
+  unCheckAll = () =>
+    (this.availablePizzaToppings = this.availablePizzaToppings.map((x) => ({
+      ...x,
+      checked: false,
+    })));
 }
