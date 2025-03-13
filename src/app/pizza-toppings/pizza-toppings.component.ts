11a51233a49bb6a049a921ccdd48ce7312e0490e
tsteelematc <tsteele@madisoncollege.edu>
2025-03-12 19:00:48 -0500

    Helping craft a cleaner PR...
    Making these 3 files match their version in `master` since they don't need to change...

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -52,16 +52,14 @@ export class PizzaToppingsComponent implements OnInit {
   checkAll = () => this.availablePizzaToppings = this.availablePizzaToppings.map(
     x => ({
       ...x
-      ,checked: true
- 
+      , checked: true
     })
   );
 
   uncheckAll = () => this.availablePizzaToppings = this.availablePizzaToppings.map(
     x => ({
       ...x
-      ,checked: false
- 
+      , checked: false
     })
   );
 
