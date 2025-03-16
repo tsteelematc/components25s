7b466e8d5252a62bef087e832c582ba8bff41bd1
Seth Gorelik <srgorelik@users.noreply.github.com>
2025-03-16 15:26:53 -0500

    Boom! Now calling a dependency injected service

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PizzaService } from '../pizza.service';
 
 @Component({
   selector: 'app-pizza-toppings',
   standalone: false,
   templateUrl: './pizza-toppings.component.html',
-  styleUrl: './pizza-toppings.component.css'
+  styleUrl: './pizza-toppings.component.css',
 })
-export class PizzaToppingsComponent {
+export class PizzaToppingsComponent implements OnInit {
+  // magic DI (dependency injection)
+  constructor(private pizzaSvc: PizzaService) {}
 
+  ngOnInit(): void {
+    const pt = this.pizzaSvc.getPizzaToppingsFromTheCloud();
+    console.log(pt);
+  }
 }
