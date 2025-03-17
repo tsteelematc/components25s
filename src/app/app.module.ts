d1df4f242be3d708dd5af4159c8c8b0184005d42
Seth Gorelik <srgorelik@users.noreply.github.com>
2025-03-17 18:00:10 -0500

    starting to build the delivery time component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,23 +10,29 @@ import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+
+// delivery time imports
 import { SrgDeliveryTimeComponent } from './srg-delivery-time/srg-delivery-time.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
     AppComponent,
     PizzaToppingsComponent,
-    SrgDeliveryTimeComponent
+    SrgDeliveryTimeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-    , FormsModule
-    , MatCardModule
-    , MatButtonModule
-    , MatCheckboxModule
+    AppRoutingModule,
+    FormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatFormFieldModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
